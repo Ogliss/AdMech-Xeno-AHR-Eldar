34b385219c16d3f507e7c8ee686a1ed50d0dcb80
Ogliss <randomdragon2@hotmail.com>
2020-02-17 17:54:58 +0000

    V1.5 Inital Commit

---
+++ Source/AdeptusMechanicusXenoEldar/AdeptusMechanicusEldarExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AbilityUser;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace AdeptusMechanicus.ExtensionMethods
+{
+
+    public static class AdeptusMechanicusEldarExtensions
+    {
+        public static bool isEldar(this Pawn pawn)
+        {
+            return pawn.def == OGEldarThingDefOf.OG_Alien_Eldar || pawn.def == OGEldarThingDefOf.Alien_Eldar;
+        }
+
+        public static bool isWraithConstruct(this Pawn pawn)
+        {
+            return pawn.RaceProps.FleshType == OGEldarThingDefOf.OG_Flesh_Construct_Eldar;
+        }
+
+        public static CompSoulStone SlotLoadable(this Pawn pawn)
+        {
+            return pawn.TryGetComp<CompSoulStone>();
+        }
+    }
+    
+}
---
+++ Source/AdeptusMechanicusXenoEldar/WraithConstructs/WraithPawn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdeptusMechanicus.ExtensionMethods;
+using Harmony;
+using Verse;
+
+namespace AdeptusMechanicus
+{
+
+    public class WraithPawn : ArtificalPawn
+    {
+        public override void ExposeData()
+        {
+            base.ExposeData();
+        }
+
+    }
+
+}
