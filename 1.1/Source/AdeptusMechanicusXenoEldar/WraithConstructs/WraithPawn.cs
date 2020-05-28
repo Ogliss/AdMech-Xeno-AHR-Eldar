0384c714e41a6897ebe53ec944f18940aecdae4d
Ogliss <randomdragon2@hotmail.com>
2020-05-28 08:30:41 +0100

    V1.5.0.1

---
+++ 1.1/Source/AdeptusMechanicusXenoEldar/WraithConstructs/WraithPawn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdeptusMechanicus.ExtensionMethods;
+using HarmonyLib;
+using Verse;
+
+namespace AdeptusMechanicus
+{
+
+    public class WraithPawn : ArtificalPawn
+    {
+        public override void ExposeData()
+        {
+            base.ExposeData();
+        }
+
+    }
+
+}
