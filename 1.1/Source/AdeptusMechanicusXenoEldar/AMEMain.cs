0a39bd40e31d1efbd271f393c5f2804daa492669
Ogliss <randomdragon2@hotmail.com>
2020-05-28 01:37:51 +0100

    V1.5.0.1

---
+++ 1.1/Source/AdeptusMechanicusXenoEldar/AMEMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdeptusMechanicus.HarmonyInstance;
+using AdeptusMechanicus.settings;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace AdeptusMechanicus
+{
+    [StaticConstructorOnStartup]
+    public class AMEMain
+    {
+        public static List<ResearchProjectDef> EldarReseach => DefDatabase<ResearchProjectDef>.AllDefs.Where(x => x.defName.Contains("OG_Eldar_Tech_")).ToList();
+        static AMEMain()
+        {
+            AlienRace.ThingDef_AlienRace eldar = OGEldarThingDefOf.OG_Alien_Eldar as AlienRace.ThingDef_AlienRace;
+            AlienRace.ThingDef_AlienRace darkeldar = DefDatabase<ThingDef>.GetNamedSilentFail("OG_Alien_Eldar") as AlienRace.ThingDef_AlienRace;
+            foreach (ResearchProjectDef def in EldarReseach)
+            {
+                if (!AlienRace.RaceRestrictionSettings.researchRestrictionDict.ContainsKey(key: def))
+                    AlienRace.RaceRestrictionSettings.researchRestrictionDict.Add(key: def, value: new List<AlienRace.ThingDef_AlienRace>());
+                AlienRace.RaceRestrictionSettings.researchRestrictionDict[key: def].Add(item: eldar);
+            }
+            HarmonyPatches.TryAddRacialRestrictions(eldar, "E");
+            if (darkeldar!=null)
+            {
+                HarmonyPatches.TryAddRacialRestrictions(darkeldar, "DE");
+            }
+            else
+            {
+                HarmonyPatches.TryAddRacialRestrictions(eldar, "DE");
+            }
+
+        }
+
+    }
+}
---
+++ 1.1/Source/AdeptusMechanicusXenoEldar/Harmony/Settings/AMXBE_AMAMod_ModOptions_Patches.cs
@@ -0,0 +1,95 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using AdeptusMechanicus.settings;
+using System.Linq;
+using AdeptusMechanicus.ExtensionMethods;
+
+namespace AdeptusMechanicus
+{
+    [HarmonyPatch(typeof(AMAMod), "ModLoaded")]
+    public static class AMXB_AMAMod_SettingsCategory_Patch
+    {
+        [HarmonyPostfix]
+        public static void ModsLoaded(ref AMAMod __instance, ref string __result)
+        {
+            __result += ", " + "AME_ModName".Translate();
+        }
+    }
+    /*
+    [HarmonyPatch(typeof(AMAMod), "SettingsCategory")]
+    public static class AME_AMAMod_SettingsCategory_Patch
+    {
+        [HarmonyPostfix, HarmonyPriority(399)]
+        public static void SettingsCategory_Postfix(ref AMAMod __instance, ref string __result)
+        {
+            __result += ", " + "AME_ModName".Translate();
+        }
+    }
+    [HarmonyPatch(typeof(AMAMod), "get_MenuLength")]
+    public static class AMO_AMAMod_MenuLength_Patch
+    {
+        [HarmonyPostfix]
+        public static void MenuLength_Postfix(ref float __result)
+        {
+            //    Log.Message(string.Format("PreModOptions_Prefix num2: {0}",  num2));
+            __result += (AMSettings.Instance.ShowOrk ? (AdeptusIntergrationUtil.enabled_MagosXenobiologis ? 60f : 120f) : 0);
+
+            //    Log.Message(string.Format("PreModOptions_Prefix num2: {0}", num2));
+        }
+
+    }
+	*/
+    [HarmonyPatch(typeof(AMAMod), "EldarSettings")]
+    public static class AME_AMMod_PlayableEldarSettings_Patch
+    {
+        [HarmonyPrefix, HarmonyPriority(401)]
+        public static bool EldarSettings_Prefix(ref AMAMod __instance, ref Listing_Standard listing_Main, Rect rect, Rect inRect, float num, float num2)
+        {
+            AMSettings settings = __instance.settings;
+            bool showRaces = settings.ShowAllowedRaceSettings;
+            bool setting = settings.ShowAllowedRaceSettings && settings.ShowEldar;
+            float lineheight = (Text.LineHeight + listing_Main.verticalSpacing);
+            float w = rect.width * 0.480f;
+            int Options = 3;
+            float RaceSettings = __instance.Length(setting, Options, lineheight, 8, showRaces ? 1 : 0); //(settings.ShowImperium ? (lineheight * 2) : (lineheight * 1)) + (settings.ShowImperium ? 10 : 0);
+            float options = __instance.Length(setting, Options - 1, lineheight, 0, 0);
+
+            Listing_Standard listing_Race = listing_Main.BeginSection(RaceSettings);
+            listing_Race.CheckboxLabeled("AMXB_ShowEldar".Translate() + " Settings" + (Prefs.DevMode && SteamUtility.SteamPersonaName.Contains("Ogliss") ? " Menu Length: " + __instance.XenobiologisEldarMenuLength : "") + (Prefs.DevMode && SteamUtility.SteamPersonaName.Contains("Ogliss") && setting ? " options length: " + options : ""), ref settings.ShowEldar, null, false, true);
+
+            if (setting)
+            {
+                Listing_Standard listing_General = listing_Race.BeginSection(options, true);
+                listing_General.ColumnWidth *= 0.488f;
+                listing_General.CheckboxLabeled("AMXB_AllowEldarCraftworld".Translate() + (!DefDatabase<FactionDef>.AllDefs.Any(x => x.defName.Contains("OG_Eldar_Craftworld")) ? "AMXB_NotYetAvailable".Translate() : "AMXB_HiddenFaction".Translate()), ref settings.AllowEldarCraftworld, null, !DefDatabase<FactionDef>.AllDefs.Any(x => x.defName.Contains("OG_Eldar_Craftworld")), DefDatabase<FactionDef>.AllDefs.Any(x => x.defName.Contains("OG_Eldar_Craftworld")));
+                listing_General.CheckboxLabeled("AMXB_AllowEldarWraithguard".Translate(), ref settings.AllowEldarWraithguard, null, !DefDatabase<ThingDef>.AllDefs.Any(x => x.defName.Contains("Wraithguard")), DefDatabase<ThingDef>.AllDefs.Any(x => x.defName.Contains("Wraithguard")));
+                listing_General.NewColumn();
+                listing_General.CheckboxLabeled("AMXB_AllowEldarExodite".Translate() + (!DefDatabase<FactionDef>.AllDefs.Any(x => x.defName.Contains("OG_Eldar_Exodite")) ? "AMXB_NotYetAvailable".Translate() : "AMXB_Faction".Translate()), ref settings.AllowEldarExodite, null, !DefDatabase<FactionDef>.AllDefs.Any(x => x.defName.Contains("OG_Eldar_Exodite")), DefDatabase<FactionDef>.AllDefs.Any(x => x.defName.Contains("OG_Eldar_Exodite")));
+                listing_General.CheckboxLabeled("AMXB_AllowEldarHarlequinn".Translate() + (!DefDatabase<FactionDef>.AllDefs.Any(x => x.defName.Contains("OG_Eldar_Harlequin")) ? "AMXB_NotYetAvailable".Translate() : "AMXB_HiddenFaction".Translate()), ref settings.AllowEldarHarlequinn, null, !DefDatabase<FactionDef>.AllDefs.Any(x => x.defName.Contains("OG_Eldar_Harlequin")), DefDatabase<FactionDef>.AllDefs.Any(x => x.defName.Contains("OG_Eldar_Harlequin")));
+                listing_Race.EndSection(listing_General);
+            }
+            listing_Main.EndSection(listing_Race);
+            __instance.XenobiologisEldarMenuLength = listing_Race.CurHeight;
+            return false;
+        }
+    }
+
+    /*
+    [HarmonyPatch(typeof(AMAMod), "PostModOptions")]
+    public static class AMO_AMAMod_PostModOptions_Patch
+    {
+        [HarmonyPostfix, HarmonyPriority(399)]
+        public static void PostModOptions_Prefix(Listing_Standard listing_Main, Rect inRect, float width, float menuLength)
+        {
+            AMSettings settings = AMSettings.Instance;
+            settings.Write();
+        }
+
+    }
+    */
+}
