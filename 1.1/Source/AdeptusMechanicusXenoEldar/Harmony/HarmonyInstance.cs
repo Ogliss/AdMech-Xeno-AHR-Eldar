57ec5c978922e1be292cc903ceffec29ec7d0b2f
Ogliss <randomdragon2@hotmail.com>
2020-10-19 13:13:01 +0100

    Tweaks n Fixes for new HAR changes

--- 1.1/Source/AdeptusMechanicusXenoEldar/Harmony/HarmonyInstance.cs
+++ 1.1/Source/AdeptusMechanicusXenoEldar/Harmony/HarmonyInstance.cs
@@ -17,6 +17,10 @@ namespace AdeptusMechanicus.HarmonyInstance
             var harmony = new Harmony("com.ogliss.rimworld.mod.AdeptusMechanicus.Eldar");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
 
+            if (AdeptusIntergrationUtil.enabled_SOS2)
+            {
+                HarmonyPatches.SOSConstructPatch();
+            }
             if (Prefs.DevMode) Log.Message(string.Format("Adeptus Xenobiologis: Eldar: successfully completed {0} harmony patches.", harmony.GetPatchedMethods().Select(new Func<MethodBase, Patches>(Harmony.GetPatchInfo)).SelectMany((Patches p) => p.Prefixes.Concat(p.Postfixes).Concat(p.Transpilers)).Count((Patch p) => p.owner.Contains(harmony.Id))), false);
         }
     }
