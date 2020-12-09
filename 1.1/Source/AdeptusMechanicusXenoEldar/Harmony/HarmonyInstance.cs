e58777e4c9fc7604a73761dd6a97d0a54a07e973
Ogliss <randomdragon2@hotmail.com>
2020-12-09 17:41:58 +0000

    AI Texture Upscale, other tweaks and fixes

--- 1.1/Source/AdeptusMechanicusXenoEldar/Harmony/HarmonyInstance.cs
+++ 1.1/Source/AdeptusMechanicusXenoEldar/Harmony/HarmonyInstance.cs
@@ -16,11 +16,12 @@ namespace AdeptusMechanicus.HarmonyInstance
         {
             var harmony = new Harmony("com.ogliss.rimworld.mod.AdeptusMechanicus.Eldar");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
-
+            /*
             if (AdeptusIntergrationUtility.enabled_SOS2)
             {
                 HarmonyPatches.SOSConstructPatch();
             }
+            */
             if (Prefs.DevMode) Log.Message(string.Format("Adeptus Xenobiologis: Eldar: successfully completed {0} harmony patches.", harmony.GetPatchedMethods().Select(new Func<MethodBase, Patches>(Harmony.GetPatchInfo)).SelectMany((Patches p) => p.Prefixes.Concat(p.Postfixes).Concat(p.Transpilers)).Count((Patch p) => p.owner.Contains(harmony.Id))), false);
         }
     }
