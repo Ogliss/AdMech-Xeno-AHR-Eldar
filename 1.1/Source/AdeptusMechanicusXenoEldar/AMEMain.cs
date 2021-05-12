e542c86cca9d3af2b618bd55605125c87acc5057
Ogliss <randomdragon2@hotmail.com>
2021-05-12 16:32:37 +0100

    Mesh, Ranger, Avenger armour tweaks

--- 1.1/Source/AdeptusMechanicusXenoEldar/AMEMain.cs
+++ 1.1/Source/AdeptusMechanicusXenoEldar/AMEMain.cs
@@ -17,21 +17,23 @@ namespace AdeptusMechanicus
         public static List<ResearchProjectDef> DarkEldarResearch => DefDatabase<ResearchProjectDef>.AllDefs.Where(x => x.defName.Contains("OG_DarkEldar_Tech_")).ToList();
         static AMEMain()
         {
-            List<string> blackTags = new List<string>() { "I", "C" };
+            List<string> blackTags = new List<string>() { "I", "C", "AM" };
             List<ResearchProjectDef> blackProjects = new List<ResearchProjectDef>();
             blackProjects.AddRange(ArmouryMain.ReseachImperial);
             blackProjects.AddRange(ArmouryMain.ReseachChaos);
 
             List<ResearchProjectDef> whiteProjects = EldarResearch;
             List<string> whiteTags = new List<string>() { "E" };
+            List<ThingDef> whiteApparel = DefDatabase<ThingDef>.AllDefsListForReading.FindAll(x => x.defName.Contains("OGE_Apparel_"));
             whiteProjects.AddRange(AeldariResearch);
             if (EldarThingDefOf.OG_Alien_DarkEldar == null)
             {
                 whiteProjects.AddRange(DarkEldarResearch);
+                whiteApparel.AddRange(DefDatabase<ThingDef>.AllDefsListForReading.FindAll(x => x.IsApparel && x.defName.Contains("OGDE_Apparel_")));
                 whiteTags.Add("DE");
             }
 
-            AlienRaceUtility.DoRacialRestrictionsFor(EldarThingDefOf.OG_Alien_Eldar, whiteTags, blackTags, whiteProjects, blackProjects);
+            AlienRaceUtility.DoRacialRestrictionsFor(EldarThingDefOf.OG_Alien_Eldar, whiteTags, blackTags, whiteProjects, blackProjects, whiteApparel, Logging: AMAMod.Dev);
         }
 
     }
